b65a1251e3f6c54becff78a621f016606876574b
De <spark49942@gmail.com>
2024-02-25 19:19:52 +0300

    flaskdz-3-v2

--- dz.py
+++ dz.py
@@ -1,18 +1,32 @@
-from flask import Flask
-from dzmi import db
+from flask import Flask, render_template, request
+from dzmi import db, Regform
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 db.init_app(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 42
+    if request.method == 'POST':
+        name = request.form.get('Name')
+        surname =  request.form.get('Surname')
+        email = request.form.get('Email')
+        password = request.form.get('Password')
+        newbie = Regform(name=f'{name}', surname=f'{surname}', email=f'{email}', password=f'{password}')
+        db.session.add(newbie)
+        db.session.commit()
+    return render_template("base.html")
 
 @app.cli.command('init-db')
 def init_db():
     db.create_all()
     print('Датабаза готова к работе.')
+
+@app.cli.command('drop-db')
+def drop_db():
+    db.drop_all()
+    print('Датабаза очищена.')
+
 
 if __name__=="__main__":
     app.run(debug=True)
--- dzmi.py
+++ dzmi.py
@@ -5,6 +5,6 @@ db = SQLAlchemy()
 class Regform(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    surename = db.Column(db.String(80), nullable=False)
+    surname = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=True, nullable=False)
